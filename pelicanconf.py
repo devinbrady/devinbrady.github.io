6ac269171dd6afd1717cf6501dd8077e05aef65b
Devin Brady <devinbrady@gmail.com>
2020-03-10 12:02:05 -0400

    Author name change

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Devin'
+AUTHOR = u'Devin Brady'
 SITENAME = u'Devin Brady Consulting'
 SITEURL = 'https://devinbrady.com'
 
